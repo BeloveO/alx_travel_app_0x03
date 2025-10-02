3cd7c04645cf2765c65eed71b323106562754f6a
Belove <beloveolusola@gmail.com>
2025-10-02 03:45:01 -0600

    Viewsets for Listing and Booking models

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from .views import ListingViewSet, BookingViewSet
+from rest_framework.routers import DefaultRouter
+from django.urls import include
+
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet, basename='listing')
+router.register(r'bookings', BookingViewSet, basename='booking')
 
 urlpatterns = [
-    # Define your URL patterns here if needed
-    path('listings/', ListingViewSet.as_view({'get': 'list', 'post': 'create'}), name='listing-list'),
-    path('listings/<int:pk>/', ListingViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='listing-detail'),
-    path('listings/<int:pk>/bookings/', ListingViewSet.as_view({'get': 'bookings', 'post': 'create_booking'}), name='listing-bookings'),
-    path('bookings/', BookingViewSet.as_view({'get': 'list', 'post': 'create'}), name='booking-list'),
-    path('bookings/<int:pk>/', BookingViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='booking-detail'),
-    path('bookings/my/', BookingViewSet.as_view({'get': 'my_bookings'}), name='my-bookings'),
-    path('bookings/host/', BookingViewSet.as_view({'get': 'host_bookings'}), name='host-bookings'),
+    path('', include(router.urls)),
 ]
