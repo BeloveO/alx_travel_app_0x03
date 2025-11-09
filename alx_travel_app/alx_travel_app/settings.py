06f94f43800d297fd88076fe17ea25d5839a3cd7
Belove Olusola <104320050+BeloveO@users.noreply.github.com>
2025-11-09 07:19:11 -0700

    Remove WhiteNoise middleware from MIDDLEWARE
    Removed WhiteNoise middleware from settings.

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # Add CORS middleware at the top
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
